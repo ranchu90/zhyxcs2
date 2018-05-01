e98e769b37610817084f3a767568cec9d15bdeda
fangyou <chendong227@qq.com>
2018-05-01 11:40:02 +0800

    MainApplication name changed from Demo to Main

--- src/main/java/com/zhyxcs/xxzz/MainApplication.java
+++ src/main/java/com/zhyxcs/xxzz/MainApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.zhyxcs.xxzz.mapper")
-public class DemoApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 }
