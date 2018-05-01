681bd76ed6c9cb8b6d44c221a5bf5bd12d33d33c
zwb <123@qq.com>
2018-05-01 16:07:31 +0800

    通用工具类

--- src/main/java/com/zhyxcs/xxzz/utils/CommonUtils.java
+++ src/main/java/com/zhyxcs/xxzz/utils/CommonUtils.java
@@ -1,9 +1,26 @@
 package com.zhyxcs.xxzz.utils;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class CommonUtils {
+    public static boolean compareString(String srcStr, String destStr) {
+        if (srcStr == null && destStr == null) {
+            return true;
+        } else {
+            if (srcStr != null && destStr == null) {
+                return false;
+            }
+            if (srcStr == null && destStr != null) {
+                return false;
+            }
+            return srcStr.equals(destStr);
+        }
+    }
+
     public static String MD5(String source) {
         StringBuilder sb = new StringBuilder();
         MessageDigest md5 = null;
@@ -19,6 +36,36 @@ public class CommonUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if (ip.equals("127.0.0.1")) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) {
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
     }
 
     public static void main(String[] args) {
