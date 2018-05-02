0fb4b2588b7a4e8968f71a0313cfed52d64780a7
zwb <123@qq.com>
2018-05-02 10:47:17 +0800

    通用工具类

---
+++ src/main/java/com/zhyxcs/xxzz/utils/CramsConstants.java
@@ -0,0 +1,6 @@
+package com.zhyxcs.xxzz.utils;
+
+public class CramsConstants {
+    public static final String SESSION_LOGIN_USER="pbc_hnxx_zhyxcs_login_user";
+    public static final String SESSION_ORGA_WITH_USER="pbc_hnxx_zhyxcs_login_orga_with_user";
+}
