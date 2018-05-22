1abd57fd3a33e048105eb5a8d64f31c5c1f9ea8b
zwb <123@qq.com>
2018-05-22 11:42:05 +0800

    日志查询分页功能完成，已测试通过20180522

---
+++ src/main/java/com/zhyxcs/xxzz/utils/StringToDateConverter.java
@@ -0,0 +1,26 @@
+package com.zhyxcs.xxzz.utils;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StringToDateConverter implements Converter<String,Date> {
+    private static final String shortDateFormat = "yyyy-MM-dd";
+
+    @Override
+    public Date convert(String value) {
+        if(StringUtils.isEmpty(value)) {
+            return null;
+        }
+        value = value.trim();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(shortDateFormat);
+        try{
+            return simpleDateFormat.parse(value);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
